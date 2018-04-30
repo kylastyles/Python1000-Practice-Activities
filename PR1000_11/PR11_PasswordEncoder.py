9bb938f5ce2af7cfd51b1b65de5067b787352060
Kyla Rieger <kyla.styles@gmail.com>
2018-04-29 22:01:04 -0400

    Completed PR11_PasswordEncoder.py

--- PR1000_11/PR11_PasswordEncoder.py
+++ PR1000_11/PR11_PasswordEncoder.py
@@ -11,65 +11,78 @@ Requirements: PT11_PasswordEncoder.pdf
 # Mission Summary: Create a "Password Encoder"
 
 
-# HELPER FUNCTIONS
+# HELPER FUNCTION
 
 def make_string_of_ords(myString):
     ord_str = ""
     for char in myString:
         x = ord(char)
-        ord_str += str(x)
+        ord_str += str(x) + ";"
     return ord_str
 
 
+# STEP 1: ENCRYPT PASSWORD
+
 def encrypt(user_id, password):
-    # Determine integral value of characters in strings
-    ord_combo = ""
-
-    id_index = 0
-    pw_index = 0
-
-    while pw_index < len(password):
-
-        x = ord(user_id[id_index])
-        y = ord(password[pw_index])
-        ord_combo += str(x+y)
-
-        id_index += 1
-        if id_index == len(user_id):
-            id_index = 0
+    """
+    Determine integral value of characters in strings, add them together (wrapping user_id if needed),
+    and return as a hex representation.
+    """
+    if user_id == "" or password == "":
+        return None
         
-        pw_index += 1
+    encrypted_str = ""
+    id_index = 0
+    for c in password:
+        if id_index >= len(user_id):
+            id_index = 0
+        combined_char = hex(ord(c) + ord(user_id[id_index]))
+        encrypted_str += combined_char + ";"
+        id_index += 1
     
-    return hex(int(ord_combo))
+    return encrypted_str
 
 
+# STEP 2: DECRYPT PASSWORD
 
 def decrypt(hex_pw, user_id):
+    """
+    Translate from hex back to ordinal value of characters, subtract user_id (wrapped if necessary) values from sum,
+    return original password.
+    """
+    
+    # Prepare user_id to work as key:
     ord_id = make_string_of_ords(user_id)
-    ord_pw = str(int(hex_pw, 16))
+    # Turn user_id and password strings into lists, deleting the erroneous ' ' at the end.
+    id_list = ord_id.split(";")
+    del id_list[-1]
+    pw_list = hex_pw.split(";")
+    del pw_list[-1]
+
     password = ""
 
-    for i in range(0, len(ord_pw), 3):
-        x = chr(int(ord_pw[i:i+3]) - int(ord_id[i:i+3]))
-        password += x 
+    # Iterate through password list, wrapping and subtracting user_id char
+    id_index = 0
+    for i in pw_list:
+        if id_index >= len(id_list):
+            id_index = 0
+        char = int(i, 16) - int(id_list[id_index])
+        password += chr(char)
+        id_index += 1 
 
     return password
 
 
-
 # TEST CASES
-user_id = "dog"
-password = "cat"
+user_id = "doggy"
+password = "kittycat"
 
 test1 = encrypt(user_id, password)
-#print(test1)
-#print(int(test1, 16))
+print(test1)
 print(decrypt(test1, user_id))
 
-# print(ord("a")) #97
-# a = 9
-# b = 7
-# print(chr(int(str(a) + str(b))))
+
+
 
 
 
